9c0dea180aec6b3727dd00e19bd3da39f4dcfaeb
PuddingNnn <puddingnnn529@gmail.com>
2016-05-11 17:16:24 -0700

    modify code structure

--- NAR.R
+++ NAR.R
@@ -1,15 +1,17 @@
-#### Please set the default directory
-
-# setwd("E:/øÏ≈Ã/netVARRound2/Report/code/")
-################################### Simulation for 3 Network Structures
-################################### ##########################################
-
-source("simulation/simulator.R")  ### source the simulator file
-source("estimator.R")
+source('NAR_helper.R')
 
 ###################### Generate three network structures
 ###################### ##########################################
 set.seed(2016)
+
+Nrep = 1000  ### number of replications
+NSize = c(100, 500, 1000)  ### network sizes
+BetaList = list(c(0.3, 0, 0.5), c(0, 0.1, -0.2), c(0.3, -0.1, 0.5))
+
+### setup for TSize (Dyad), block size (Block), and alpha (power-law)
+TrueParaList = list(c(10, 30, 100), c(5, 10, 20), c(1.2, 2, 3))
+gamma0 = c(-0.5, 0.3, 0.8, 0, 0)  ### true parameter for gamma
+ZSigma = 0.5^abs(outer(1:5, 1:5, "-"))  ### Sigma_z (covariance for Z)
 
 #### Dyad Independence network P(Dij = (1,1)) = 2N1/N; P(Dij = (1,0)) =
 #### P(Dij = (0,1)) = 0.5*N^{-0.8}
@@ -48,7 +50,7 @@ G = beta[2] * W + beta[3] * diag(1, nrow(W))
 Z = mvrnorm(n = 500, mu = rep(0, nrow(ZSigma)), Sigma = ZSigma)  ## Z~N(0, ZSigma)
 
 Ymat = simu.data(W, beta0 = beta[1], Beta = beta[2:3], Time = 10, G = G, 
-    Z = Z, sig = 1)
+                 Z = Z, sig = 1)
 
 par(mfrow = c(1, 2))
 plot(colMeans(Ymat), type = "o", xlab = "Time", ylab = "Average Responses")
@@ -58,23 +60,3 @@ hist(rowSums(Ymat), col = "dodgerblue", xlab = "Responses of Nodes")
 
 ThetaEstOLS = betaOLS(Ymat, W, Z)  ### estimate for theta
 str(ThetaEstOLS)
-
-###################### Real Data Analysis ##########################################
-
-### transform the csv to R sparse matrix
-
-source("real_data/realDataFunc.R")
-netS = getNetStruc(file = "../data/netStr.csv")
-str(netS)
-Amat = netS$Amat
-W = Amat/rowSums(Amat)  ### obtain W matrix
-
-######################################## NAR Fit from Week 1 to 4 ####################
-
-logYmat = as.matrix(read.csv("../data/logYmat.csv"))
-nodal = read.csv("../data/nodalCovariate.csv")
-nodal$gender = as.numeric(nodal$gender)
-
-train = getRegDf(logYmat, W, nodal)  ### the regression dataset
-reg = lm(Y ~ ., data = train)
-summary(reg) 
